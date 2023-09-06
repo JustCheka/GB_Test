a97bfed80cd48d04ffcf43787f7c086aa9685973
JustCheka <cheka201@yandxe.ru>
2023-09-06 16:16:56 +0300

    Added program

---
+++ SolveProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите длину массива ");
+int len = int.Parse(Console.ReadLine());
+string[] strArr = new string[len];
+
+for (int index = 0; index < len; index++){
+    Console.Write($"Введите {index + 1} элемент массива ");
+    strArr[index] = Console.ReadLine();
+}
+
+int countOfLen3 = 0;
+for (int index = 0; index < len; index++){
+    if (strArr[index].Length <= 3) countOfLen3++;
+}
+
+if (countOfLen3 != 0){
+    string[] newStrArr = new string[countOfLen3];
+    int index2 = 0;
+    for (int index = 0; index < len; index++){
+        if (strArr[index].Length <= 3){
+            newStrArr[index2] = strArr[index];
+            index2++;
+        }
+    }
+
+    Console.WriteLine();
+    foreach(string el in newStrArr){
+        Console.Write(el + " ");
+    }
+}
+else {
+    Console.WriteLine("\nНет строк, длина которых меньше, либо равна 3 символам");
+}
